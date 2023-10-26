0a6b88bd5af99c71263d6a588d0b1c31d4f9caec
zhennongchen <chenzhennong@gmail.com>
2023-10-26 13:06:57 -0400

    finish fine tune motion

--- Generator_motion.py
+++ Generator_motion.py
@@ -4,11 +4,15 @@ import numpy as np
 import random
 import os
 import nibabel as nb
-from scipy.ndimage.measurements import center_of_mass
+
 from tensorflow.keras.utils import Sequence
 import DeepStrain.functions_collection as ff
 import DeepStrain.Defaults as Defaults
 import DeepStrain.Data_processing as Data_processing
+
+from DeepStrain.data.nifti_dataset import resample_nifti
+from DeepStrain.data.base_dataset import _roll2center_crop
+from scipy.ndimage.measurements import center_of_mass
 
 cg = Defaults.Parameters()
 
@@ -68,50 +72,180 @@ class DataGenerator(Sequence):
         batch_v0_output = np.zeros(tuple([self.batch_size]) + tuple([self.img_shape[0],self.img_shape[1], self.img_shape[2]]) + (1,))
         batch_m0_output = np.zeros(tuple([self.batch_size]) + tuple([self.img_shape[0],self.img_shape[1], self.img_shape[2]]) + (3,))
 
-        # load template frame:
-        v0_file = os.path.join(self.image_folder, 'Org3D_frame' + str(self.template_tf) + '.nii.gz' )
-        m0_file = os.path.join(self.seg_folder, 'pred_seg_frame' + str(self.template_tf) + '.nii.gz' )
-        if os.path.isfile(m0_file) == 0:
-            m0_file = os.path.join(self.seg_folder, 'SAX_ED_seg.nii.gz' )
-        v0 = nb.load(v0_file).get_fdata()[:,:,self.heart_slices[0]:self.heart_slices[1]]
-        m0 = np.round(nb.load(m0_file).get_fdata()[:,:,self.heart_slices[0]:self.heart_slices[1]]).astype(np.int)
-        center = center_of_mass(m0==2)
-        v0 = Data_processing.crop_or_pad(v0[int(center[0]) - 64 : int(center[0]) + 64, int(center[1]) - 64 : int(center[1]) + 64, :], self.img_shape, value = np.min(v0))
-        m0 = Data_processing.crop_or_pad(m0[int(center[0]) - 64 : int(center[0]) + 64, int(center[1]) - 64 : int(center[1]) + 64, :], self.img_shape, value = 0)
-
-        if self.normalize is not None:
-            v0 = ff.normalize(v0)
-        v0 = v0[...,np.newaxis]
-        m0 = Data_processing.one_hot(m0, num_classes = 3)
-        # print('v0 shape: ', v0.shape, 'm0 shape: ', m0.shape, 'max and min of v0: ', np.max(v0), np.min(v0), 'unique of m0: ', np.unique(m0))
         
         for i, j in enumerate(indexes):
-            # load template time frame
             delta = self.target_tf_delta[j]
             target_tf = (self.template_tf + delta)
             if target_tf > 25:
                 target_tf = target_tf - 25
             # print('index here: ', i, j,delta, ' target time frame: ', target_tf)
+
+            v0_file = os.path.join(self.image_folder, 'Org3D_frame' + str(self.template_tf) + '.nii.gz' )
+            m0_file = os.path.join(self.seg_folder, 'pred_seg_frame' + str(self.template_tf) + '.nii.gz' )
+            if os.path.isfile(m0_file) == 0:
+                m0_file = os.path.join(self.seg_folder, 'SAX_ED_seg.nii.gz' )
+
             vt_file = os.path.join(self.image_folder, 'Org3D_frame' + str(target_tf) + '.nii.gz' )
             mt_file = os.path.join(self.seg_folder, 'pred_seg_frame' + str(target_tf) + '.nii.gz' )
             if os.path.isfile(mt_file) == 0:
                 mt_file = os.path.join(self.seg_folder, 'SAX_ES_seg.nii.gz' )
-            vt = nb.load(vt_file).get_fdata()[:,:,self.heart_slices[0]:self.heart_slices[1]]
-            mt = np.round(nb.load(mt_file).get_fdata()[:,:,self.heart_slices[0]:self.heart_slices[1]]).astype(np.int)
-            vt = Data_processing.crop_or_pad(vt[int(center[0]) - 64 : int(center[0]) + 64, int(center[1]) - 64 : int(center[1]) + 64, :], self.img_shape, value = np.min(vt))
-            mt = Data_processing.crop_or_pad(mt[int(center[0]) - 64 : int(center[0]) + 64, int(center[1]) - 64 : int(center[1]) + 64, :], self.img_shape, value = 0)
 
-            if self.normalize is not None:
-                vt = ff.normalize(vt)
-            vt = vt[...,np.newaxis]
-            mt = Data_processing.one_hot(mt, num_classes = 3)
-            # print('vt shape: ', vt.shape, 'mt shape: ', mt.shape, 'max and min of vt: ', np.max(vt), np.min(vt), 'unique of mt: ', np.unique(mt))
+            V_nifti_ED = nb.load(v0_file)
+            V_nifti_ES = nb.load(vt_file)
+            M_nifti_ED = nb.load(m0_file)
+            M_nifti_ES = nb.load(mt_file)
 
-            batch_v0_input[i] = v0
-            batch_vt_input[i] = vt
-            batch_mt_input[i] = mt
-            batch_v0_output[i] = v0
-            batch_m0_output[i] = m0
+            M_ED = np.round(M_nifti_ED.get_fdata()).astype(int)
+            M_nifti_ED = nb.Nifti1Image(M_ED, affine=M_nifti_ED.affine, header=M_nifti_ED.header)
+            M_ES = np.round(M_nifti_ES.get_fdata()).astype(int)
+            M_nifti_ES = nb.Nifti1Image(M_ES, affine=M_nifti_ES.affine, header=M_nifti_ES.header)
 
-        # return [batch_v0_input, batch_vt_input], [batch_MVF_output]
+            # fourth: prepare input
+            V_nifti = nb.funcs.concat_images((V_nifti_ED, V_nifti_ES))
+            M_nifti = nb.funcs.concat_images((M_nifti_ED, M_nifti_ES))
+
+            V_nifti = resample_nifti(V_nifti, order=1, in_plane_resolution_mm=1.25, number_of_slices=16)
+            M_nifti = resample_nifti(M_nifti, order=0, in_plane_resolution_mm=1.25, number_of_slices=16)
+
+            # calculate center of mass using the first frame as reference. This is needed for cropping to 128x128
+            center = center_of_mass(M_nifti.get_fdata()[:,:,:,0]==2) # RV = 1, Myocardium = 2, LV = 3
+            print('center: ',center)
+            V = _roll2center_crop(x=V_nifti.get_fdata(), center=center)
+            M = _roll2center_crop(x=M_nifti.get_fdata(), center=center)
+      
+            # 
+            V = ff.normalize_image(V)
+            nx, ny, nz, nt = V.shape
+            V_0 =  np.repeat(V[:,:,:,:1], nt-1, axis=-1)
+            V_t =  V[:,:,:,1:]
+            M_0 =  Data_processing.one_hot(np.round(M[:,:,:,0]).astype(np.int), num_classes = 3)
+            M_t =  Data_processing.one_hot(np.round(M[:,:,:,0]).astype(np.int), num_classes = 3)
+            # print('after normalize, shape: ',V_0.shape, V_t.shape, M_0.shape, M_t.shape, ' Labels: ', np.unique(M_t))
+
+            batch_v0_input[i] = V_0
+            batch_vt_input[i] = V_t
+            batch_mt_input[i] = M_t
+            batch_v0_output[i] = V_0
+            batch_m0_output[i] = M_0
+            # batch_MVF_output[i] = nb.load(os.path.join(cg.deep_dir, 'models/fine_tune_carmen/ID_0015/mvf.nii.gz')).get_fdata()
         return [batch_v0_input, batch_vt_input, batch_mt_input], [batch_MVF_output, batch_v0_output, batch_m0_output]
+    
+
+# class DataGenerator(Sequence):
+
+#     def __init__(self,
+#                 patient_id,
+#                 image_folder,
+#                 seg_folder,
+#                 template_tf,
+#                 target_tf_delta,  # target time frame - template time frame
+#                 heart_slices, 
+#                 batch_size = 1,
+#                 img_shape = None,        
+#                 normalize = None,
+#                 seed = 10):
+        
+#         self.patient_id = patient_id
+#         self.image_folder = image_folder
+#         self.seg_folder = seg_folder
+#         self.template_tf = template_tf
+#         self.target_tf_delta = target_tf_delta
+#         self.heart_slices = heart_slices
+#         self.batch_size = batch_size
+#         self.img_shape = img_shape
+#         self.normalize = normalize
+#         self.seed = seed
+
+#         self.on_epoch_end()
+        
+#     def __len__(self):
+#         return self.target_tf_delta.shape[0] // (self.batch_size)
+
+#     def on_epoch_end(self):
+#         self.seed += 1
+
+#         self.indices = np.arange(self.target_tf_delta.shape[0])
+
+#         # print('all indexes: ', self.indices,len(self.indices))
+
+#     def __getitem__(self,index):
+
+#         # 'Generate indexes of the batch'
+#         current_index = (index * self.batch_size) % self.target_tf_delta.shape[0]
+        
+#         indexes = self.indices[current_index : current_index + self.batch_size]
+
+#         # print('indexes in this batch: ',indexes)
+
+#         # allocate memory
+#         batch_v0_input = np.zeros(tuple([self.batch_size]) + tuple([self.img_shape[0],self.img_shape[1], self.img_shape[2]]) + (1,))
+#         batch_vt_input = np.zeros(tuple([self.batch_size]) + tuple([self.img_shape[0],self.img_shape[1], self.img_shape[2]]) + (1,))
+#         batch_mt_input = np.zeros(tuple([self.batch_size]) + tuple([self.img_shape[0],self.img_shape[1], self.img_shape[2]]) + (3,))
+
+#         batch_MVF_output = np.zeros(tuple([self.batch_size]) + tuple([self.img_shape[0],self.img_shape[1], self.img_shape[2]]) + (3,))
+#         batch_v0_output = np.zeros(tuple([self.batch_size]) + tuple([self.img_shape[0],self.img_shape[1], self.img_shape[2]]) + (1,))
+#         batch_m0_output = np.zeros(tuple([self.batch_size]) + tuple([self.img_shape[0],self.img_shape[1], self.img_shape[2]]) + (3,))
+
+#         # load template frame:
+#         v0_file = os.path.join(self.image_folder, 'Org3D_frame' + str(self.template_tf) + '.nii.gz' )
+#         m0_file = os.path.join(self.seg_folder, 'pred_seg_frame' + str(self.template_tf) + '.nii.gz' )
+#         if os.path.isfile(m0_file) == 0:
+#             m0_file = os.path.join(self.seg_folder, 'SAX_ED_seg.nii.gz' )
+            
+#         v0_nii = nb.load(v0_file)
+#         m0_nii = nb.load(m0_file)
+
+#         v0_nii = resample_nifti(v0_nii, order=1, in_plane_resolution_mm=1.25, number_of_slices=16)
+#         m0_nii = resample_nifti(m0_nii, order=0, in_plane_resolution_mm=1.25, number_of_slices=16)
+
+#         v0 = v0_nii.get_fdata()
+#         m0 = np.round(m0_nii.get_fdata()).astype(np.int)
+#         center = center_of_mass(m0==2)
+#         v0 = v0[int(center[0]) - 64 : int(center[0]) + 64, int(center[1]) - 64 : int(center[1]) + 64, :]
+#         m0 = m0[int(center[0]) - 64 : int(center[0]) + 64, int(center[1]) - 64 : int(center[1]) + 64, :]
+
+#         if self.normalize is not None:
+#             v0 = ff.normalize(v0)
+#         v0 = v0[...,np.newaxis]
+#         m0 = Data_processing.one_hot(m0, num_classes = 3)
+#         # print('v0 shape: ', v0.shape, 'm0 shape: ', m0.shape, 'max and min of v0: ', np.max(v0), np.min(v0), 'unique of m0: ', np.unique(m0))
+        
+#         for i, j in enumerate(indexes):
+#             # load template time frame
+#             delta = self.target_tf_delta[j]
+#             target_tf = (self.template_tf + delta)
+#             if target_tf > 25:
+#                 target_tf = target_tf - 25
+#             # print('index here: ', i, j,delta, ' target time frame: ', target_tf)
+#             vt_file = os.path.join(self.image_folder, 'Org3D_frame' + str(target_tf) + '.nii.gz' )
+#             mt_file = os.path.join(self.seg_folder, 'pred_seg_frame' + str(target_tf) + '.nii.gz' )
+#             if os.path.isfile(mt_file) == 0:
+#                 mt_file = os.path.join(self.seg_folder, 'SAX_ES_seg.nii.gz' )
+
+#             vt_nii = nb.load(vt_file)
+#             mt_nii = nb.load(mt_file)
+#             vt_nii = resample_nifti(vt_nii, order=1, in_plane_resolution_mm=1.25, number_of_slices=16)
+#             mt_nii = resample_nifti(mt_nii, order=0, in_plane_resolution_mm=1.25, number_of_slices=16)
+
+#             vt = vt_nii.get_fdata()
+#             mt = np.round(mt_nii.get_fdata()).astype(np.int)
+#             vt = vt[int(center[0]) - 64 : int(center[0]) + 64, int(center[1]) - 64 : int(center[1]) + 64, :]
+#             mt = mt[int(center[0]) - 64 : int(center[0]) + 64, int(center[1]) - 64 : int(center[1]) + 64, :]
+
+
+#             if self.normalize is not None:
+#                 vt = ff.normalize(vt)
+#             vt = vt[...,np.newaxis]
+#             mt = Data_processing.one_hot(mt, num_classes = 3)
+#         #     # print('vt shape: ', vt.shape, 'mt shape: ', mt.shape, 'max and min of vt: ', np.max(vt), np.min(vt), 'unique of mt: ', np.unique(mt))
+
+#             batch_v0_input[i] = v0
+#             batch_vt_input[i] = vt
+#             batch_mt_input[i] = mt
+#             batch_v0_output[i] = v0
+#             batch_m0_output[i] = m0
+#             batch_MVF_output[i] = nb.load(os.path.join(cg.deep_dir, 'models/fine_tune_carmen/ID_0015/mvf.nii.gz')).get_fdata()
+
+#         return [batch_v0_input, batch_vt_input], [batch_MVF_output]
+#         # return [batch_v0_input, batch_vt_input, batch_mt_input], [batch_MVF_output, batch_v0_output, batch_m0_output]
+
--- main_train_motion.py
+++ main_train_motion.py
@@ -33,7 +33,7 @@ cg = Defaults.Parameters()
 trial_name = 'fine_tune_carmen'
 data_sheet = os.path.join(cg.deep_dir,'data/Patient_list','Patient_list_for_motion_checked.xlsx')
 patient_index = 0
-maximum_epoch = 20
+maximum_epoch = 300
 ##############################################
 
 # build list
@@ -49,77 +49,73 @@ M_t_input = Input(shape = [128,128,16,3])
 M_t_split = tf.split(M_t_input, M_t_input.shape[-1], -1)
 
 input   = Concatenate(axis=-1)([V_0_input, V_t_input])
-print('input shape: ',input.shape)
 motion_estimates = network.encoder_decoder(input, nchannels=3, map_activation=None)
 V_0_pred = warp(V_t_input, motion_estimates, name = 'warp' )
 M_0_pred  = K.concatenate([warp(K.cast(mt, K.dtype(V_t_input)), motion_estimates) for mt in M_t_split], -1)    
 M_0_pred  = softmax(M_0_pred, name = 'softmax') 
-print('shape: ', V_0_pred.shape, M_0_pred.shape)
 
 model = Model(inputs = [V_0_input, V_t_input, M_t_input], outputs = [motion_estimates,V_0_pred, M_0_pred])
-# model = Model(inputs = [V_0_input, V_t_input], outputs = [motion_estimates])
 
 model_file =  os.path.join(cg.deep_dir,'models/trained/carmen_Jan2021.h5')
-# model_file = os.path.join(cg.deep_dir,'models/fine_tune_carmen/ID_0015/models/model-003.hdf5')
 model.load_weights(model_file)
 
 
-# # compile model
-# opt = Adam(lr = 1e-4)
-# model.compile(optimizer= opt, 
-#                 loss = [hyper.loss_smooth, hyper.loss_mae, hyper.loss_dice],
-#                 loss_weights=[0,0,1])
+# compile model
+opt = Adam(lr = 1e-4)
+model.compile(optimizer= opt, 
+                loss = [hyper.loss_smooth, 'MAE', hyper.loss_dice],
+                loss_weights=[0.1, 0.1, 0.5])
 
-# # # set callbacks
-# model_fld = os.path.join(cg.deep_dir, 'models', trial_name, patient_id, 'models')
-# ff.make_folder([os.path.dirname(os.path.dirname(model_fld)), os.path.dirname(model_fld), model_fld, os.path.join(os.path.dirname(model_fld), 'logs')])
-# csv_logger = CSVLogger(os.path.join(os.path.dirname(model_fld), 'logs', 'training-log.csv')) 
+# # set callbacks
+model_fld = os.path.join(cg.deep_dir, 'models', trial_name, patient_id, 'models')
+ff.make_folder([os.path.dirname(os.path.dirname(model_fld)), os.path.dirname(model_fld), model_fld, os.path.join(os.path.dirname(model_fld), 'logs')])
+csv_logger = CSVLogger(os.path.join(os.path.dirname(model_fld), 'logs', 'training-log.csv')) 
 
-# class CustomCallback(Callback):
-#     def __init__(self, model_fld, max_epoch, stopping_patience = 5):
-#         super(CustomCallback, self).__init__()
-#         self.model_fld = model_fld
-#         self.no_improvement_count = 0
-#         self.min_loss = float('inf')
-#         self.max_epoch = max_epoch
-#         self.stopping_patience = stopping_patience
+class CustomCallback(Callback):
+    def __init__(self, model_fld, max_epoch, stopping_patience = 5):
+        super(CustomCallback, self).__init__()
+        self.model_fld = model_fld
+        self.no_improvement_count = 0
+        self.min_loss = float('inf')
+        self.max_epoch = max_epoch
+        self.stopping_patience = stopping_patience
 
-#     def on_epoch_end(self, epoch, logs=None):
+    def on_epoch_end(self, epoch, logs=None):
    
-#         current_loss = logs.get('loss')
-#         if current_loss < self.min_loss:
-#             self.min_loss = current_loss
-#             self.no_improvement_count = 0
-#         else:
-#             self.no_improvement_count += 1
+        current_loss = logs.get('loss')
+        if current_loss < self.min_loss:
+            self.min_loss = current_loss
+            self.no_improvement_count = 0
+        else:
+            self.no_improvement_count += 1
 
-#         if self.no_improvement_count >= self.stopping_patience or epoch == self.max_epoch - 1:  # early stopping
-#             epoch_str = str(epoch + 1).zfill(3)  # Format epoch number
-#             print(epoch_str)
-#             self.model.stop_training = True
-#             save_model(self.model, os.path.join(self.model_fld,  'model-'+epoch_str+'.hdf5'))  # Save the last model
+        if self.no_improvement_count >= self.stopping_patience or epoch == self.max_epoch - 1:  # early stopping
+            epoch_str = str(epoch + 1).zfill(3)  # Format epoch number
+            print(epoch_str)
+            self.model.stop_training = True
+            save_model(self.model, os.path.join(self.model_fld,  'model-'+epoch_str+'.hdf5'))  # Save the last model
 
-# callbacks = [csv_logger,
-#                 CustomCallback(model_fld,maximum_epoch), 
-#                     LearningRateScheduler(hyper.learning_rate_step_decay_slower),   
-#                     ]
+callbacks = [csv_logger,
+                CustomCallback(model_fld,maximum_epoch), 
+                    LearningRateScheduler(hyper.learning_rate_step_decay_slower),   
+                    ]
 
-# # # Fit
-# datagen = Generator_motion.DataGenerator(patient_id,
-#                                         image_folder,
-#                                         seg_folder,
-#                                         ed,
-#                                         np.array([es - ed]),
-#                                         heart_slices = [start_slice, end_slice],
-#                                         batch_size = 1,
-#                                         img_shape = [128,128,16],        
-#                                         normalize = True,)
+# # Fit
+datagen = Generator_motion.DataGenerator(patient_id,
+                                        image_folder,
+                                        seg_folder,
+                                        ed,
+                                        np.array([es - ed]),
+                                        heart_slices = [start_slice, end_slice],
+                                        batch_size = 1,
+                                        img_shape = [128,128,16],        
+                                        normalize = True,)
 
 
-# model.fit_generator(generator = datagen,
-#                     epochs = 300,
-#                     callbacks = callbacks,
-#                     verbose = 1,)
+model.fit_generator(generator = datagen,
+                    epochs = 300,
+                    callbacks = callbacks,
+                    verbose = 1,)
 
 
 
